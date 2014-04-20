18ee91cc11df79bdc89c54b1c9b9e7966e51a531
Emily Lu Wang <emilyluwang@Emilys-MacBook-Air-3.local>
2014-04-20 00:39:17 -0400

    trying something with reuters

--- parser.py
+++ parser.py
@@ -8,9 +8,8 @@ import requests
 
 
 #an alternate way of getting all of a webpage
-response = urllib2.urlopen("http://www.reuters.com/article/2014/04/20/us-ukraine-crisis-idUSBREA3A1B520140420")
+response = urllib2.urlopen("http://www.cnn.com/2014/04/19/world/asia/malaysia-airlines-plane/index.html?hpt=hp_t2")
 page_source = response.read()
-
 soup = BeautifulSoup(page_source)
 
 for link in soup.find_all('p'):
