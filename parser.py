a562335a6b4588c7b6a365a14f91a384c32dbf39
Ankit Gupta <gupta.v.ankit@gmail.com>
2014-04-20 01:11:55 -0400

    lols

--- parser.py
+++ parser.py
@@ -11,10 +11,12 @@ import requests
 
 response = urllib2.urlopen("http://www.cnn.com/2014/04/19/world/asia/south-korea-ship-sinking/index.html?hpt=hp_t1")
 
+#get a page
 page_source = response.read()
 soup = BeautifulSoup(page_source)
 page = ""
 
+#get a paragraph and then remove all of the tags in it
 for para in soup.find_all('p'):
     page += para.getText()
 
