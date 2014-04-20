c28f5b333153a0c39a9610530ec77349f27ef224
Hirsh Jain <hirshjain32@gmail.com>
2014-04-20 01:36:57 -0400

    cleaned up code

--- parser.py
+++ parser.py
@@ -5,16 +5,13 @@ from collections import defaultdict
 from collections import Counter
 
 
-#r = requests.get("http://www.npatel.me")
-
-#data = r.text
-
 
 #an alternate way of getting all of a webpage
 response = urllib2.urlopen("http://www.cnn.com/2014/04/19/world/asia/south-korea-ship-sinking/index.html?hpt=hp_t1")
 
 #get a page
 page_source = response.read()
+
 
 soup = BeautifulSoup(page_source)
 
@@ -29,17 +26,3 @@ print page
 count = Counter(page.split())
 
 print count
-
-
-
-#wordlist = page.split()
-#print wordlist
-
-#assert(0)
-#wordsFreq = {}
-#for a in wordlist:
-#	wordsFreq[a] = 0
-
-#for a in wordlist:
-#	wordsFreq[a] += 1
-#print wordsFreq		
