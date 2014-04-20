70f487e1cbca431d0ba1c61bb577c2ff83f280e5
Hirsh Jain <hirshjain32@gmail.com>
2014-04-20 01:12:16 -0400

    cleaned code

--- parser.py
+++ parser.py
@@ -2,17 +2,12 @@ from bs4 import BeautifulSoup
 import urllib2
 import requests
 
-#r = requests.get("http://www.npatel.me")
-
-#data = r.text
-
-
-#an alternate way of getting all of a webpage
-
 response = urllib2.urlopen("http://www.cnn.com/2014/04/19/world/asia/south-korea-ship-sinking/index.html?hpt=hp_t1")
 
 page_source = response.read()
+
 soup = BeautifulSoup(page_source)
+
 page = ""
 
 for para in soup.find_all('p'):
