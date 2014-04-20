47a8e2fe3b4961d71f3aaf9d458ce923347a0343
Hirsh Jain <hirshjain32@gmail.com>
2014-04-20 00:23:02 -0400

    installed beautifulsoup

--- parser.py
+++ parser.py
@@ -1,14 +1,24 @@
-from HTMLParser import HTMLParser
+from bs4 import BeautifulSoup
 
+import requests
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print "Encountered a start tag:", tag
-    def handle_endtag(self, tag):
-        print "Encountered an end tag :", tag
-    def handle_data(self, data):
-        print "Encountered some data: ", data
+r = requests.get("http://www.npatel.me")
 
-parser = MyHTMLParser()
-parser.feed('<html><head><title>Test</title></head>'
-            '<body><h1>Parse me!</h1></body></html>')
+data = r.text
+
+soup = BeautifulSoup(data)
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
+
+#class MyHTMLParser(HTMLParser):
+#    def handle_starttag(self, tag, attrs):
+#        print "Encountered a start tag:", tag
+#    def handle_endtag(self, tag):
+#        print "Encountered an end tag :", tag
+#    def handle_data(self, data):
+#        print "Encountered some data: ", data
+
+#parser = MyHTMLParser()
+#parser.feed('<html><head><title>Test</title></head>'
+#            '<body><h1>Parse me!</h1></body></html>')
