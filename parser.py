63110d6e9548e20c522174d994ba9301efca400a
Hirsh Jain <hirshjain32@gmail.com>
2014-04-20 01:10:37 -0400

    simpler implementation of text scraper

--- parser.py
+++ parser.py
@@ -9,15 +9,13 @@ import requests
 
 #an alternate way of getting all of a webpage
 
-response = urllib2.urlopen("http://www.reuters.com/article/2014/04/18/us-china-petrochemicals-campaign-idUSBREA3H07020140418")
+response = urllib2.urlopen("http://www.cnn.com/2014/04/19/world/asia/south-korea-ship-sinking/index.html?hpt=hp_t1")
 
 page_source = response.read()
 soup = BeautifulSoup(page_source)
+page = ""
 
-filter_paras = ""
-for link in soup.find_all('p'):
-    filter_paras += str(link) 
-    #link += filter_paras
-soup = BeautifulSoup(filter_paras)
-#print filter_paras    
+for para in soup.find_all('p'):
+    page += para.getText()
 
+print page
