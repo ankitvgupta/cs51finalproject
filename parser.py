3f4d87849ff8ec88fa83f606e052df3025499839
Ankit Gupta <gupta.v.ankit@gmail.com>
2014-04-20 01:46:06 -0400

    made it into a function:

--- parser.py
+++ parser.py
@@ -6,25 +6,27 @@ from collections import Counter
 
 
 
-#an alternate way of getting all of a webpage
-response = urllib2.urlopen("http://www.cnn.com/2014/04/19/world/asia/south-korea-ship-sinking/index.html?hpt=hp_t1")
+def parse_page(url):
+	#an alternate way of getting all of a webpage
+	response = urllib2.urlopen(url)
 
-#reads in info
-page_source = response.read()
+	#reads in info
+	page_source = response.read()
 
-#turns page into parseable form
-soup = BeautifulSoup(page_source)
-page_text = ""
+	#turns page into parseable form
+	soup = BeautifulSoup(page_source)
+	page_text = ""
 
-#gets text from all of page
-for para in soup.find_all('p'):
-    page_text += str(para.getText())
+	#gets text from all of page
+	for para in soup.find_all('p'):
+	    page_text += str(para.getText())
 
-print page_text
+	print page_text
 
-#creates dictionary with word counts
-count = Counter(page_text.split())
-standard_dict_count = dict(count)
+	#creates dictionary with word counts
+	count = Counter(page_text.split())
+	standard_dict_count = dict(count)
+	#print count
+	return standard_dict_count
 
-print count
-print standard_dict_count
+print parse_page("http://www.cnn.com/2014/04/19/world/asia/south-korea-ship-sinking/index.html?hpt=hp_t1")
