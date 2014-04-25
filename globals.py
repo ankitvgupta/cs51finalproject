16707cf6a4c8dccda13f1afcb16fcdee96d8e6d0
Ankit Gupta <gupta.v.ankit@gmail.com>
2014-04-25 10:09:52 -0400

    removed wrong file names

--- globals.py
+++
@@ -1,9 +0,0 @@
-
-
-def init():
-	global liberal_file
-	liberal_file = 'liberal.txt'
-	global conservative_file
-	conservative_file = 'conservative.txt'
-	global test_file
-	test_file = 'testcases.txt'
