5b6d3632bf4f740cf914079e8197a8e41d11244f
Ankit Gupta <gupta.v.ankit@gmail.com>
2014-04-24 22:36:26 -0400

    added frequencies

--- parser.py
+++ parser.py
@@ -21,11 +21,20 @@ def parse_page(url):
 	for para in soup.find_all('p'):
 	    page_text += str(para.getText())
 
-	print page_text
+	#print page_text
 
 	#creates dictionary with word counts
 	count = Counter(page_text.split())
 	standard_dict_count = dict(count)
+	total_num_words = sum(standard_dict_count.values())
+
+	#change to frequencies
+	for word in standard_dict_count:
+		standard_dict_count[word] /= float(total_num_words)
+
+	print standard_dict_count	
+
+
 	#print count
 	return standard_dict_count
 
