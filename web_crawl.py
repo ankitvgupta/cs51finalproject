a66734d10a6482194552fdc3876fbe6fe490c30e
Ankit Gupta <gupta.v.ankit@gmail.com>
2014-04-30 21:20:47 -0400

    added basic web crawling

--- web_crawl.py
+++ web_crawl.py
@@ -4,39 +4,30 @@ from urlparse import urljoin
 from urlparse import urlparse
 from collections import deque
 
+starting_url = 'http://www.cnn.com/2014/04/29/politics/shumlin-midterms/index.html?hpt=po_c1'
 
+def extract_base(url):
 
-url = 'http://www.cnn.com/2014/04/29/politics/shumlin-midterms/index.html?hpt=po_c1'
+  breakdown_url = urlparse(url)
+  new_url = breakdown_url.scheme + "://" + breakdown_url.netloc
+  return new_url
 
-breakdown_url = urlparse(url)
-new_url = breakdown_url.scheme + "://" + breakdown_url.netloc
-
-def crawler(queue, websites):
+def crawler(queue, websites, base):
   if len(websites) == 100:
     return websites 
   link = queue.popleft() 
   try: 
     response = urllib2.urlopen(link)
   except:
-    return crawler(queue, websites)
+    return crawler(queue, websites, base)
   page_source = response.read()
   soup = BeautifulSoup(page_source)
-  a_tags = soup.select('a[href^=new_url]')
+  a_tags = soup.select('a[href^=%s]' % base)
   for i in a_tags:
     if ('href' in dict(i.attrs)):
       if str(i['href']) not in websites:
         queue.append(str(i['href']))
   websites.append(link)
-  return crawler(queue, websites)
+  return crawler(queue, websites, base)
 
-print crawler(deque([url]), [])
-
-
-
-
-
-
-
-
-
-
+print crawler(deque([starting_url]), [], extract_base(starting_url))
