d0e78a85402f8884ad6e5731bfad0672b4a64ab9
Nishant Kakar <nkakar@college.harvard.edu>
2014-04-30 20:55:03 -0400

    implemented a webcrawler wooo

--- web_crawl.py
+++ web_crawl.py
@@ -2,28 +2,38 @@ from bs4 import BeautifulSoup
 import urllib2
 from urlparse import urljoin
 from urlparse import urlparse
+from collections import deque
 
 
 
 url = 'http://www.cnn.com/2014/04/29/politics/shumlin-midterms/index.html?hpt=po_c1'
-response = urllib2.urlopen(url)
 
-  # reads in info
-page_source = response.read()
+def crawler(queue, websites):
+  if len(websites) == 100:
+    return websites 
+  link = queue.popleft() 
+  try: 
+    response = urllib2.urlopen(link)
+  except:
+    return crawler(queue, websites)
+  page_source = response.read()
+  soup = BeautifulSoup(page_source)
+  a_tags = soup.select('a[href^="http://www.cnn.com/"]')
+  for i in a_tags:
+    if ('href' in dict(i.attrs)):
+      if str(i['href']) not in websites:
+        queue.append(str(i['href']))
+  websites.append(link)
+  return crawler(queue, websites)
 
-  # turns page into parseable form
-soup = BeautifulSoup(page_source)
-print soup
+print crawler(deque([url]), [])
 
-links = soup.select('a[href^="http://www.cnn.com/"]')
-newlist = []
-for i in links:
-	if ('href' in dict(i.attrs)):
-		print i['href']
-	#print links
-	newlist.append(urlparse(str(i['href'])))
 
-print newlist
+
+
+
+
+
 
 
 
